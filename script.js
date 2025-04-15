b3a01d09dba01c5c45097bc7ea7a2cc84261c424
andriansyah01995 <andriansyah01995@gmail.com>
2025-04-15 10:58:15 +0700

    feat: enhance script functionality and styling, improve navigation responsiveness, and update CSS variables

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
-document.getElementById('hamburger').addEventListener('click', function () {
-    document.getElementById('nav').classList.toggle('active');
+// script.js
+document.addEventListener("DOMContentLoaded", function () {
+    const hamburger = document.getElementById("hamburger");
+    const nav = document.getElementById("nav");
+  
+    hamburger.addEventListener("click", function () {
+      nav.classList.toggle("active");
+    });
   });
   
   function openPdf(fileName) {
-    window.open(fileName, '_blank');
+    const win = window.open(fileName, '_blank');
+    if (win) {
+      win.focus();
+    } else {
+      alert('Silakan izinkan pop-up di browser Anda.');
+    }
   }
   
